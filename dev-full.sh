1111e404a0affc772ae3157c5518d61318cf2b8f
Scott Sullivan <scott@sullysbrain.com>
2025-05-11 21:31:23 -0400

    combination of docker-compose and pandas added

--- dev-full.sh
+++ dev-full.sh
@@ -3,7 +3,7 @@
 docker build -f .devcontainer/Dockerfile . -t my-analytics-image
 
 # Live Bind Mount
-docker run -it --mount type=bind,src="$(pwd)",target=/src --workdir /src my-analytics-image sh
+# docker run -it --rm --mount type=bind,src="$(pwd)",target=/src --workdir /src my-analytics-image sh
 
 # Copy to Production instead
 # docker run -it --rm my-analytics-image sh
--- src/test.py
+++ src/test.py
@@ -1,7 +1,3 @@
 # import pandas as pd
 
 print("Hello World! 42")
-print("hello from the outside world")
-
-
-print("hello from the inside world")
